22dafc774b7654b02983914caf0ca972e221295a
shadabzafar <shadabzafar2838@gmail.com>
2024-09-08 16:29:50 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def predict():
     final_input = scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output = regmodel.predict(final_input)[0]
-    return render_template("home.html", prediction_text = f"The House price prediction is {round(output,2)}")
+    return render_template("home.html", prediction_text = f"The House price prediction is ${round(output,2)}K")
 
 if __name__ == "__main__":
     app.run(debug=True)
